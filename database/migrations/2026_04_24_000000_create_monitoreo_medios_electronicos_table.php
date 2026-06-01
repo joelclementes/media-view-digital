1a8d223436c24be96573639f2d9368f1f173e054
joelclementes <joelclementes.dev@gmail.com>
2026-06-01 11:33:09 -0600

    Act migracion para cualitativos

--- database/migrations/2026_04_24_000000_create_monitoreo_medios_electronicos_table.php
+++ database/migrations/2026_04_24_000000_create_monitoreo_medios_electronicos_table.php
@@ -29,6 +29,17 @@ return new class extends Migration
             $table->string('referencia')->nullable();
             $table->string('observaciones')->nullable();
             $table->json('archivos')->nullable();
+
+            $table->string('cuali_valoracion')->comment('Positiva/Negativa/Neutral');
+            $table->string('cuali_lenguaje_inclusivo')->comment('Si,No');
+            $table->string('cuali_estereotipo')->comment('NA/Personas indígenas/Creencias religiosas de las personas/Personas afroamericanas... ');
+            $table->foreignId('cuali_violencia_temas_id')->constrained('violencia_temas')->comment('"Igualdad de género"; relación con violencia_temas');
+            $table->foreignId('cuali_tipos_eleccion_id')->constrained('tipos_eleccion')->comment('"Candidatura"; relación con tipos_eleccion');
+            $table->text('cuali_resumen');
+            $table->string('cuali_modalidad')->comment('Politica/Electoral');
+            $table->text('cuali_objetividad');
+            $table->string('cuali_tipo_mensaje')->comment('A favor/Descalificativo/Crítica/Imparcial');
+            $table->string('cuali_formato')->comment('Mensaje/De entrevista/Informativo-narrativo');
             $table->timestamps();
         });
     }
